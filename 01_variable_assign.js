a2f05ae364916eb3aadbf04c924508a2cd89ff68
poojaaldar1 <poojaaldar@gmail.com>
2022-12-09 07:07:25 +0530

    Create 01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,23 @@
+
+var myName = "Pooja Aldar";
+console.log(myName);
+
+var myName = "Pooja Bhimrao Aldar";
+console.log(myName);
+
+var myLove = "i love only JS.";
+console.log(myLove);
+
+var age = "25";
+
+
+var address = "Navi Mumbai"
+
+
+var gender = "Female"
+
+var married = "No"
+console.log(married, gender, age, address, myLove)
+
+
+
