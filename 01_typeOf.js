928567b8fbd9c0c70d01ffa56d5beeee1ae03408
poojaaldar1 <poojaaldar@gmail.com>
2022-12-10 07:17:48 +0530

    Create 01_typeOf.js

---
+++ 01_typeOf.js
@@ -0,0 +1,32 @@
+
+console.log("==== value before intialisation ====");
+var bankName;
+console.log(bankName);
+
+console.log("==== value after intialisation with data type====");
+var bankName = "SBI Bank";
+console.log(bankName);
+console.log( typeof bankName);
+
+console.log("==== value after intialisation with data type ====");
+var marks = '90%';
+console.log(marks);
+console.log( typeof marks);
+
+console.log("==== value before intialisation ====");
+var isWoking
+console.log(isWoking);
+
+console.log("==== value after intialisation with data type ====");
+isWoking = true
+console.log(isWoking);
+console.log(typeof isWoking);
+
+console.log("==== value after intialisation with data type ====");
+var totalCount 
+console.log(totalCount = "Hundred and Seven");
+console.log( typeof totalCount);
+
+console.log("==== value with data type ====");
+totalCount =107
+console.log( typeof totalCount);
