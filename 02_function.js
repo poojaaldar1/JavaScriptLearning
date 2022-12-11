c2d05f7633e532df8a52b983f46389b11690b5cd
poojaaldar1 <poojaaldar@gmail.com>
2022-12-11 11:27:00 +0530

    Update 02_function.js

---
+++ 02_function.js
@@ -0,0 +1,26 @@
+
+function show() {
+
+    console.log("My Collage Name: TKIET Warananagar");
+    console.log("my Name is Pooja");
+}
+
+show();
+
+function swapValue(value1,value2){
+    console.log( "before swap", value1,value2);
+temp=value1
+value1=value2
+value2=temp
+console.log("after swap",value1,value2);
+}
+
+swapValue(100,300);
+swapValue("you","Me");
+swapValue(2,4);
+swapValue("Amit","Pooja");
+
+function hello(myName){
+    console.log(myName);
+    }
+    hello('pooja');
