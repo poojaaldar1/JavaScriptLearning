8c4906664732004f5403b5dfc9d150596d4e0116
poojaaldar1 <poojaaldar@gmail.com>
2022-12-13 07:38:47 +0530

    Create 02_normal_assign02.js

---
+++ 02_normal_assign02.js
@@ -0,0 +1,8 @@
+console.log("---Normal Function Assignment---");
+function bankDetails (bankName, accountNum , location, pinCode) {
+
+console.log("BankName:-", bankName, "Account No:-",accountNum ,"Location:-" ,location, "Pincode:-",pinCode);
+}
+bankDetails("CITY BANK", 234567890, "pune", 431202);
+bankDetails("AXIS BANK", 234567890, "mumbai", 431202);
+bankDetails("HDFC BANK", 234567890, "pune", 431202 , "open");
