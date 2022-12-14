e7a6c79769bfa578089280178b90752f20834660
poojaaldar1 <poojaaldar@gmail.com>
2022-12-14 11:03:58 +0530

    Create 03_stringTemplate_assign01.js

---
+++ 03_stringTemplate_assign01.js
@@ -0,0 +1,20 @@
+
+console.log("_________String Assignment 01______");
+
+function stringTemplateAssign(){ 
+console.log("----1.Dream Comapny-----");
+
+console.log(`My Dream Company Name Is"TCS".`);
+console.log("---------------------------------------------");
+
+console.log("----2.My Hobbies-----");
+
+var myHobby1= "Watching Movies";
+var myHobby2= "Listening Song";
+var myHobby3= "Travelling";
+console.log("Using variable Substitution:",`My Hobbies are ${myHobby1},${myHobby2},${myHobby3}`);
+
+console.log("Using Concat Methos:","My Hobbies are", myHobby1.concat(myHobby2),myHobby3);
+
+}
+stringTemplateAssign()
