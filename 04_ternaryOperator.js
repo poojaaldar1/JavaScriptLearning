76f8551a9a9598b4ace67cc2be165183363c8951
poojaaldar1 <poojaaldar@gmail.com>
2022-12-16 23:44:58 +0530

    Create 04_ternaryOperator.js

---
+++ 04_ternaryOperator.js
@@ -0,0 +1,8 @@
+
+// If SSC marks greater than equal to 35 then pass or fail
+
+var sscMarks=34;
+
+var resultPassorFail= sscMarks>=35 ? "pass" : "fails";
+
+console.log(`Result is ${resultPassorFail}`);
