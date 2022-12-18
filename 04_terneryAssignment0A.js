85ba59f366881234486cdc41f50acb1fdb205206
poojaaldar1 <poojaaldar@gmail.com>
2022-12-18 15:45:58 +0530

    Update 04_terneryAssignment0A.js

--- 04_terneryAssignment0A.js
+++ 04_terneryAssignment0A.js
@@ -1,11 +1,19 @@
-
-function maleMarriageEligibily(gender,age,boyname){
+console.log("-------1.Boy marriage eligibility --------");
+function maleMarriageEligibily(gender,age,boyName){
 
 var result= male=gender && age>=21 ? "eligible for marriage" : "not eligible for marriage";
-console.log(`Hey ${boyname}, you are ${result}.`);
+console.log(`Hey ${boyName}, you are ${result}.`);
 
 }
 maleMarriageEligibily ("Male",25,"Billgates");
 maleMarriageEligibily ("Male",17,"Stew Jobs");
+console.log("--------------------------------------------------");
+console.log("-------2.Girl marriage eligibility --------");
+function femaleMarriageEligibily(gender,age,girlName){
 
-
+    var result= female=gender && age>=21 ? `Hey ${girlName} eligible for marriage` : "not eligible for marriage";
+    console.log(`${girlName},you are ${result}.`);
+    
+    }
+    femaleMarriageEligibily ("feMale",16,"Jenifer");
+    femaleMarriageEligibily ("feMale",27,"Stew Jobs");
