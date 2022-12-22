7adb5f80c3b90d3e870e3a4ec0328dc4ebd3a789
poojaaldar1 <poojaaldar@gmail.com>
2022-12-22 07:15:39 +0530

    Update 05_loopStatement_Assign0B.js

--- 05_loopStatement_Assign0B.js
+++ 05_loopStatement_Assign0B.js
@@ -1,30 +1,29 @@
-       
-       console.log("1. Printing Numbers from 5 to 15 increamenting 1");
-       for (let index = 5; index <=15; index++) {
-        console.log(index);
-          }
-    
-    console.log("2. Printing Numbers from 50 to 40 decreamenting 1");
-    for (let index = 50; index <=40; --index) {
-        console.log(index);
-    }
-    console.log("3. first odd 15 numbers");
-    for (let index = 1; index <=15; index=index+2) {
-        console.log(index);
-    }
-    console.log("4. first even 10 numbers");
-    for (let index = 2; index <=10; index=index+2) {
-        console.log(index);
-    }
-    console.log("5. Table 5 ");
-    for (let index = 5; index <=50; index=index+5) {
-        console.log(index);
-    }
-    console.log("6. Table 10 ");
-    for (let index = 10; index <=100; index=index+10) {
-        console.log(index);
-    }
-    console.log("7. Table 10 in reverse direction");
-    for (let index = 100; index <=10; index=index-10) {
-        console.log(index);
-    }
+console.log("1. Printing Numbers from 5 to 15 increamenting 1");
+for (let index = 5; index <= 15; index++) {
+  console.log(index);
+}
+
+console.log("2. Printing Numbers from 50 to 40 decreamenting 1");
+for (let index = 50; index >= 40; index--) {
+  console.log(index);
+}
+console.log("3. first odd 15 numbers");
+for (let index = 1; index <= 15; index = index + 2) {
+  console.log(index);
+}
+console.log("4. first even 10 numbers");
+for (let index = 2; index <= 10; index = index + 2) {
+  console.log(index);
+}
+console.log("5. Table 5 ");
+for (let index = 5; index <= 50; index = index + 5) {
+  console.log(index);
+}
+console.log("6. Table 10 ");
+for (let index = 10; index <= 100; index = index + 10) {
+  console.log(index);
+}
+console.log("7. Table 10 in reverse direction");
+for (let index = 100; index >= 10; index = index - 10) {
+  console.log(index);
+}
