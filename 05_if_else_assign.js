3ee449c5ba1f0aa5a41932bcb7f225022cf64ef5
poojaaldar1 <poojaaldar@gmail.com>
2022-12-25 09:19:48 +0530

    Update 05_if_else_assign.js

--- 05_if_else_assign.js
+++ 05_if_else_assign.js
@@ -24,36 +24,32 @@ console.log("\n");
 
 console.log(`.......1.Marks with Grades system.......`);
 
-var gradeCalculation = function (marks){
+var gradeCalculation = function (marks) {
   // (marks == 0 || marks < 0 || marks > 100 || typeof marks != "number"   OR)
 
-if (marks==0 || marks>100 || typeof marks!="number") {
-  console.log(`Your marks is ${marks}, Please enter valid Data`);
-  
-} else {
-  if (marks>90 && marks<=100) {
-    console.log(`Funtastic Marks :${marks} "your grade is A+"`);
-  }
-else{
-  if (marks>=75 && marks<90) {
-    console.log(`Excellent marks: ${marks} "your grade is A"`);
+  if (marks == 0 || marks > 100 || typeof marks != "number") {
+    console.log(`Your marks is ${marks}, Please enter valid Data`);
   } else {
-    if (marks>=50 && marks<75) {
-      console.log(`Good marks: ${marks} "your grade is B"`);
+    if (marks > 90 && marks <= 100) {
+      console.log(`Funtastic Marks :${marks} "your grade is A+"`);
     } else {
-      if (marks>=35 && marks<50) {
-        console.log(`Marks is: ${marks} "your grade is C, Need Improvment"`);
+      if (marks >= 75 && marks < 90) {
+        console.log(`Excellent marks: ${marks} "your grade is A"`);
       } else {
-        
+        if (marks >= 50 && marks < 75) {
+          console.log(`Good marks: ${marks} "your grade is B"`);
+        } else {
+          if (marks >= 35 && marks < 50) {
+            console.log(
+              `Marks is: ${marks} "your grade is C, Need Improvment"`
+            );
+          } else {
+          }
+        }
       }
-      
     }
   }
-}
-}
-
-
-}
+};
 
 gradeCalculation(98);
 gradeCalculation(80);
@@ -68,26 +64,32 @@ gradeCalculation(49);
 gradeCalculation(91);
 gradeCalculation("eighty");
 
+console.log(
+  `---------------------------------------------------------------------------------------------------`
+);
 
-console.log(`---------------------------------------------------------------------------------------------------`);
- 
-var marriageEligibilty = function (gender , age){
+// var male >=21;
+// var female>=18;
 
-  if (male<21 & female<18) {
-      console.log(`Gender:${gender} & Age:${age}, Sorry your not eligible for marriage.`);
+var marriageEligibilty = function (name,gender, age) {
+  if (gender == "male" && age >= 21) {
+    console.log(
+      `Age:${age}&Gender:${gender}:- Hey ${name} Congrats!! You are eligible for marriage.`
+    );
   } else {
-      if (male>21||female>18) {
-        console.log(`Gender:${gender} & Age:${age}, Sorry your not eligible for marriage.`);
-      }
-      if (male) {
-        
-      }
-  }
+    if (gender == "female" && age >= 18) {
+      console.log(
+        `Age:${age}&Gender:${gender}:- Hey ${name} Congrats!! You are eligible for marriage.`
+      );
+    } else if (gender!="male"||gender!="female") {
+     console.log(`Gender:${gender}:- Sorry ${name}, You are not eligible for marriage.`); 
+    } 
+    }
+  };
 
-}
-marriageEligibilty ("male", 17);
-marriageEligibilty ("male", 17);
-marriageEligibilty ("male", 17);
-marriageEligibilty ("male", 17);
-marriageEligibilty ("male", 17);
-marriageEligibilty ("male", 17);
+marriageEligibilty("Poonit","male",17);
+marriageEligibilty("Sachin","male", 25);
+marriageEligibilty("Pooja","female", 28);
+marriageEligibilty("Rajani","female", 16);
+marriageEligibilty("Laxmi","other", 35);
+marriageEligibilty("Rani","others", 41);
