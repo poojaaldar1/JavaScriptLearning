ddcd5546e96ca9e5bf3d4c1e1eddaafb2aec23e1
poojaaldar1 <poojaaldar@gmail.com>
2022-12-29 08:57:21 +0530

    Create 07_objectAssign.js

---
+++ 07_objectAssign.js
@@ -0,0 +1,30 @@
+const professor = {
+  name: "Mr.Rakesh Sharma",
+  age: 45,
+  Mobile: 8620031112,
+  dregree: {
+      engineering: "CSC",
+      PHD : "Adv Computiong"
+      },
+      add : function(){
+         return this.engineering+this.PHD;
+      }
+   certificates:['HackerRank Participation','Certificate in IFE course','Certificate in adv Programming']
+};
+
+console.log(`------------------Professor Details------------------`);
+professor.name = "Mr.Rakesh Sharma";
+professor.id= 555;
+professor.gender= "male"
+professor.age = 45;
+professor.contact = 8690961100;
+console.log(`Name of the Professor is :${professor.name}`);
+console.log(`Name of the Professor is :${professor.id}`);
+console.log(`Name of the Professor is :${professor.gender= "male"}`);
+console.log(`Age of the Professor is :${professor.age}`);
+console.log(`Age of the Professor is :${professor.contact}`);
+
+
+
+
+
