c225e15554894d2bebdfa681c82f4adb4a7aba39
poojaaldar1 <poojaaldar@gmail.com>
2022-12-31 06:40:49 +0530

    Create 07_functionConstructor.js

---
+++ 07_functionConstructor.js
@@ -0,0 +1,14 @@
+
+function Student(name, rollNo, age){
+    this.name = name;
+    this.rollNo = rollNo;
+    this.age = age;
+ }
+ let s1 = new Student("Vinny", 111, 23);
+ console.log(s1.rollNo);
+ let s2 = new Student("Jenny", 22, 35);
+ console.log(s1);
+
+let isInstance = studChetan instanceof Employee;
+console.log(isInstance);
+// Object is an instance of class
