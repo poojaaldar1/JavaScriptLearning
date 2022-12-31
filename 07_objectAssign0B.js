2045f36c6c52e769015681de08a543aa84a1c829
poojaaldar1 <poojaaldar@gmail.com>
2022-12-31 06:41:03 +0530

    Create 07_objectAssign0B.js

---
+++ 07_objectAssign0B.js
@@ -0,0 +1,37 @@
+
+
+const bankDetials= {
+sbiBank :{
+bankName: b5132356565323246,
+Location: "Mumbai",
+accountNo: 265632355664335,
+ifsc: indexedDB6565,
+intrestRate: 5%
+},
+
+axisBank :{
+bankName: b5132356565323246,
+Location: "Mumbai",
+accountNo: 265632355664335,
+ifsc: indexedDB6565,
+intrestRate: 5%
+},
+
+hdfcBank :{
+bankName: b5132356565323246,
+Location: "Mumbai",
+accountNo: 265632355664335,
+ifsc: indexedDB6565,
+intrestRate: 5%
+},
+
+yesBank :{
+bankName: b5132356565323246,
+Location: "Mumbai",   
+accountNo: 265632355664335,
+ifsc: indexedDB6565,
+intrestRate: 5%
+}
+}
+
+
