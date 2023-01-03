4c86aa08251c6657ebca8b408d4ed7fa85dc29bf
poojaaldar1 <poojaaldar@gmail.com>
2023-01-03 07:06:05 +0530

    Update 07_objectAssign0B.js

--- 07_objectAssign0B.js
+++ 07_objectAssign0B.js
@@ -1,37 +1,76 @@
+var bankDetails = {
+  sbiBank: {
+    Location: "Mumbai",
+    accountNo: 265632355664335,
+    ifsc: "indexedDB6565",
+    intrestRate: 5,
+    showDetails: function () {
+      return (
+        this.bankName +
+        this.location +
+        this.accountNo +
+        this.ifsc +
+        this.intrestRate
+      );
+    },
+  },
 
+  axisBank: {
+    Location: "Mumbai",
+    accountNo: 265632355664335,
+    ifsc: "indexedDB6565",
+    intrestRate: 5,
+    showDetails: function () {
+      this.bankName +
+        this.location +
+        this.accountNo +
+        this.ifsc +
+        this.intrestRate;
+    },
+  },
 
-const bankDetials= {
-sbiBank :{
-bankName: b5132356565323246,
-Location: "Mumbai",
-accountNo: 265632355664335,
-ifsc: indexedDB6565,
-intrestRate: 5%
-},
+  hdfcBank: {
+    Location: "Mumbai",
+    accountNo: 265632355664335,
+    ifsc: "indexedDB6565",
+    intrestRate: 5,
+    showDetails: function () {
+      this.bankName +
+        this.location +
+        this.accountNo +
+        this.ifsc +
+        this.intrestRate;
+    },
+  },
 
-axisBank :{
-bankName: b5132356565323246,
-Location: "Mumbai",
-accountNo: 265632355664335,
-ifsc: indexedDB6565,
-intrestRate: 5%
-},
+  yesBank: {
+    Location: "Mumbai",
+    accountNo: 265632355664335,
+    ifsc: "indexedDB6565",
+    intrestRate: 5,
+    showDetails: function () {
+        return this.location + this.accountNo +this.ifsc +this.intrestRate;
+       
+    }
 
-hdfcBank :{
-bankName: b5132356565323246,
-Location: "Mumbai",
-accountNo: 265632355664335,
-ifsc: indexedDB6565,
-intrestRate: 5%
-},
+  },
+};
 
-yesBank :{
-bankName: b5132356565323246,
-Location: "Mumbai",   
-accountNo: 265632355664335,
-ifsc: indexedDB6565,
-intrestRate: 5%
-}
-}
+console.log(`Bank Names are:`);
+console.log(bankDetails);
 
+console.log(`1.------------YES BAnk Details------------`);
+var yesB= bankDetails.yesBank;
+console.log(yesB);
 
+console.log(`2.------------hdfcBank BAnk Details------------`);
+var hdfcB= bankDetails.hdfcBank;
+console.log(hdfcB);
+
+console.log(`3.------------axisBank BAnk Details------------`);
+var axisB= bankDetails.axisBank;
+console.log(axisB);
+
+console.log(`4.------------sbiBank BAnk Details------------`);
+var sbiB= bankDetails.sbiBank;
+console.log(sbiB);
