d361f91d47cee6c4d0c3e6ef52448ced6eff57d8
poojaaldar1 <poojaaldar@gmail.com>
2023-01-03 07:05:59 +0530

    Update 05_vowelAssignment.js

--- 05_vowelAssignment.js
+++ 05_vowelAssignment.js
@@ -1,39 +1,40 @@
 // var sentence = "I AM Learining JavaScript,The Language of internet";
 
-//  function  countCharA(words) {
-//     for (let index = 0; index < sentence.length; index++) {
-//        var count =sentence.charAt(index)
-//         if (count=="a" && count=="A") {
-//             console.log(count);
-           
-//         }
-//     }
-// }
-// var count = countCharA("a")
-// console.log(`Total vowels in string is : ${count}`);
+ function countcharA(string) {
+  var counter=0
 
-// var sentence = "I AM Learining JavaScript,The Language of internet";
-// // Print the vowels only aeiou or AEIOU
-
-
-
-
-
-
-
-
-
-
-
-var sentence = "My favourite movie is LAggAn";
-var counter = 0;
-for (let index = 0; index <= sentence.length; index++) {
-  var char= sentence.charAt(index);
-  if (char==`a`||char==`e`||char==`i`||char==`o`||char==`u`||
-  char==`A`||char==`E`||char==`I`||char==`O`||char==`U`) 
-  {
-    console.log(char);
-counter = counter+1;
+for (let index = 0; index < string.length; index++) {
+    var count =string.charAt(index)
+    if (count=="a" || count=="A") {
+    console.log(` ${string}: ${count}`);
+    counter = counter+1
+    console.log(`Total number of a& A in given string is : ${counter}`);
+    
+                    }
+    }
   }
-}
-console.log(`Total Chara of the string is ${counter}`); 
+    countcharA ("I AM Learining JavaScript,The Language of internet");
+    countcharA ("My favourite movie is LAggAn");
+
+
+
+
+
+
+
+
+
+
+
+// var sentence = "My favourite movie is LAggAn";
+// var counter = 0;
+// for (let index = 0; index <= sentence.length; index++) {
+//   var char= sentence.charAt(index);
+//   if (char==`a`||char==`e`||char==`i`||char==`o`||char==`u`||
+//   char==`A`||char==`E`||char==`I`||char==`O`||char==`U`) 
+//   {
+//     console.log(char);
+// counter = counter+1;
+//   }
+// }
+// console.log(`Total Chara of the string is ${counter}`); 
