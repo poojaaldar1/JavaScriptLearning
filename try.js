0c864cbedc95beb7b8998328dd4e6726025f0810
poojaaldar1 <poojaaldar@gmail.com>
2023-01-05 23:42:37 +0530

    Create try.js

---
+++ try.js
@@ -0,0 +1,11 @@
+
+const stdName= new set ();
+
+stdName.add("chetan");
+stdName.add("hitesh");
+stdName.add("varsha");
+stdName.add("komal");
+stdName.add("pooja");
+stdName.add("reeta");
+
+console.log(stdName);
